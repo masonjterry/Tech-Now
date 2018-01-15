7d1a37e219d3249d1cdb5fa8d5f446ee833d1e4a
Mason Terry <masonjterry@Masons-Air.attlocal.net>
2018-01-14 21:19:06 -0600

    creating intial file sturcture

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const logger = require("morgan");
-// const exphbs = require("express-handlebars");
-// const axios = require("axios");
-// const cheerio = require("cheerio");
-// const db = require("./models");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const exphbs = require("express-handlebars");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require("./models");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// app.use(logger("dev"));
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(express.statis("public"));
-//
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-//
-// require("./controllers/techNowController")(app);
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.statis("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./controllers/techNowController")(app);
 
 mongoose.Promise = Promise;
 
